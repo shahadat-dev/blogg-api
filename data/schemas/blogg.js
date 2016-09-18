947ff7a3d9a719ee0671477fd32be2bff724ec61
Shahadat Hossain <shahadat.cseru@gmail.com>
2016-09-18 15:31:23 +0600

    change blogg schema

--- data/schemas/blogg.js
+++ data/schemas/blogg.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var bloggSchema = new mongoose.Schema({
-	name: { type: String, required: true},
-	description: { type: String }
+	//category: { type: Schema.Types.ObjectId },
+	post: { type: String, required: true},
+	comments: { type: Schema.Types.Mixed },
+	upvote: { type: Number, default: 0 },
+	downvote: { type: Number, default: 0 },
+	created_at:{ type: Date, default: Date.now },
+	updated_at:{ type: Date, default: null }
 });
 
 module.exports = bloggSchema;
